3d5e22423d440b30a89aa92bfedd06d97f7f8ee8
Todd Stout <todd.tstout@gmail.com>
2019-05-19 16:37:07 -0500

    initial customization

---
+++ init.el
@@ -0,0 +1,42 @@
+;; fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
+(when window-system (set-exec-path-from-shell-PATH))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(defvar my-packages '(projectile
+                      clojure-mode
+                      cider
+		      dracula-theme))
+
+;; (defvar my-packages '(better-defaults
+;;                       projectile
+;;                       clojure-mode
+;;                       cider))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(show-paren-mode 1)      ;; Odd that this is not the default
+(global-linum-mode t)
