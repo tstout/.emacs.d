e21d9a40da2e5dc2c1bba384e514ccc59e79e17c
Todd Stout <btstout@containerstore.com>
2020-05-22 15:20:41 -0500

    capture

--- init.el
+++ init.el
@@ -1,40 +1,47 @@
 ;; fix the PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
+;; (defun set-exec-path-from-shell-PATH ()
+;;   (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+;;     (setenv "PATH" path-from-shell)
+;;     (setq exec-path (split-string path-from-shell path-separator))))
 
-(when window-system (set-exec-path-from-shell-PATH))
+;; (when window-system (set-exec-path-from-shell-PATH))
 
 (require 'package)
 (add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
-(defvar my-packages '(projectile
-                      clojure-mode
-                      cider
-		      dracula-theme))
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+;; (defvar my-packages '(projectile
+;;                       clojure-mode
+;;                       cider
+;; 		      dracula-theme))
 
 ;; (defvar my-packages '(better-defaults
 ;;                       projectile
 ;;                       clojure-mode
 ;;                       cider))
 
-(dolist (p my-packages)
-  (unless (package-installed-p p)
-    (package-install p)))
+;; (dolist (p my-packages)
+;;   (unless (package-installed-p p)
+;;     (package-install p)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(custom-enabled-themes (quote (dracula)))
  '(custom-safe-themes
    (quote
     ("b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default)))
- '(package-selected-packages (quote (better-defaults))))
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell markdown-mode dracula-theme ac-cider better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -44,3 +51,4 @@
 
 (show-paren-mode 1)      ;; Odd that this is not the default
 (global-linum-mode t)
+(desktop-save-mode 1)
